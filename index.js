3b7f40bb7867854bfdccf55b9f59c80ca7b7edb9
Maurizio Zucca <mauriziozucca@Mac-mini-di-Maurizio.local>
2022-05-30 12:13:52 +0200

    add invaliddate API endpoint

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,13 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date_string", (res, req) => {
-  res.json({ error: "Invalid Date" })
+app.get("/api/timestamp/:date_string", function (req, res) {
+  res.json({error : 'Invalid Date' });
 })
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
